cd8d487b7a42d815f5a4200ff02904ac469c0c88
seonghoo1217 <lsh6451217@gmail.com>
2025-02-15 00:31:19 +0900

    spring 환경 library setting

---
+++ src/main/java/task/prography10th/Prography10thApplication.java
@@ -0,0 +1,13 @@
+package task.prography10th;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prography10thApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prography10thApplication.class, args);
+    }
+
+}
---
+++ src/test/java/task/prography10th/Prography10thApplicationTests.java
@@ -0,0 +1,13 @@
+package task.prography10th;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prography10thApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
