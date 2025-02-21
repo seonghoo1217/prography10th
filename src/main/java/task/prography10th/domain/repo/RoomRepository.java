02920eac7fa65aade85647c438d8a48db4bd4bb6
seonghoo1217 <lsh6451217@gmail.com>
2025-02-22 00:25:26 +0900

    feat: JPA Repository 구현

---
+++ src/main/java/task/prography10th/domain/repo/RoomRepository.java
@@ -0,0 +1,7 @@
+package task.prography10th.domain.repo;
+
+import org.springframework.data.repository.Repository;
+import task.prography10th.domain.room.Room;
+
+public interface RoomRepository extends Repository<Room, Integer> {
+}
---
+++ src/main/java/task/prography10th/domain/repo/UserRepository.java
@@ -0,0 +1,7 @@
+package task.prography10th.domain.repo;
+
+import org.springframework.data.repository.Repository;
+import task.prography10th.domain.user.User;
+
+public interface UserRepository extends Repository<User, Integer> {
+}
