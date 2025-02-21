64e830ea3000bcd76061ff3974daf06db832f244
seonghoo1217 <lsh6451217@gmail.com>
2025-02-21 19:05:51 +0900

    refactor: API Response Class Type으로 변경

--- src/main/java/task/prography10th/global/dto/ApiResponse.java
+++ src/main/java/task/prography10th/global/dto/ApiResponse.java
@@ -1,20 +1,32 @@
 package task.prography10th.global.dto;
 
-public record ApiResponse<T>(Integer code, String message, T result) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
 
-    public static <T> ApiResponse<T> of (Integer code, String message, T result){
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public final class ApiResponse<T> {
+    private final Integer code;
+    private final String message;
+    private T result;
+
+    public ApiResponse(Integer code, String message, T result) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    public ApiResponse(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public static <T> ApiResponse<T> of(Integer code, String message, T result) {
         return new ApiResponse<>(code, message, result);
     }
 
-    public static <T> ApiResponse<T> success(T result) {
-        return of(200, "API 요청이 성공했습니다.", result);
-    }
-
-    public static <T> ApiResponse<T> badRequest(T result) {
-        return of(201, "불가능한 요청입니다.", result);
-    }
-
-    public static <T> ApiResponse<T> error(T result) {
-        return of(500, "에러가 발생했습니다.", result);
+    public static <T> ApiResponse<T> of(Integer code, String message) {
+        return new ApiResponse<>(code, message);
     }
 }
