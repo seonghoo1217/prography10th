19fed28cb582ca041e70a6d28dd6e809a6c308b7
seonghoo1217 <lsh6451217@gmail.com>
2025-02-21 21:35:11 +0900

    feat: 헬스체크 API 구현

---
+++ src/main/java/task/prography10th/presentation/HealthCheckAPI.java
@@ -0,0 +1,18 @@
+package task.prography10th.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import task.prography10th.global.dto.ApiResponse;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("health")
+public class HealthCheckAPI {
+
+    @GetMapping
+    public ApiResponse<?> healthCheck(){
+        return ApiResponse.success();
+    }
+}
