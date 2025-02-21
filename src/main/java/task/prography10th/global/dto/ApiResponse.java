605906bdecd6772ec0d7eebf520228a4c2897b73
seonghoo1217 <lsh6451217@gmail.com>
2025-02-21 18:10:56 +0900

    refactor: API Response 상태코드별 메서드 분리

--- src/main/java/task/prography10th/global/dto/ApiResponse.java
+++ src/main/java/task/prography10th/global/dto/ApiResponse.java
@@ -1,7 +1,20 @@
 package task.prography10th.global.dto;
 
 public record ApiResponse<T>(Integer code, String message, T result) {
+
     public static <T> ApiResponse<T> of (Integer code, String message, T result){
         return new ApiResponse<>(code, message, result);
+    }
+
+    public static <T> ApiResponse<T> success(T result) {
+        return of(200, "API 요청이 성공했습니다.", result);
+    }
+
+    public static <T> ApiResponse<T> badRequest(T result) {
+        return of(201, "불가능한 요청입니다.", result);
+    }
+
+    public static <T> ApiResponse<T> error(T result) {
+        return of(500, "에러가 발생했습니다.", result);
     }
 }
