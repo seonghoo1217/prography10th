0833bcf781be8f98e871100516bc66656886492b
seonghoo1217 <lsh6451217@gmail.com>
2025-02-21 21:34:52 +0900

    refactor: API Response 상태에 따른 내부메서드 추가

--- src/main/java/task/prography10th/global/dto/ApiResponse.java
+++ src/main/java/task/prography10th/global/dto/ApiResponse.java
@@ -22,11 +22,23 @@ public final class ApiResponse<T> {
         this.message = message;
     }
 
-    public static <T> ApiResponse<T> of(Integer code, String message, T result) {
+    private static <T> ApiResponse<T> of(Integer code, String message, T result) {
         return new ApiResponse<>(code, message, result);
     }
 
-    public static <T> ApiResponse<T> of(Integer code, String message) {
+    private static <T> ApiResponse<T> of(Integer code, String message) {
         return new ApiResponse<>(code, message);
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return of(200, "API 요청이 성공했습니다.");
+    }
+
+    public static <T> ApiResponse<T> badRequest(T result) {
+        return of(201, "불가능한 요청입니다.", result);
+    }
+
+    public static <T> ApiResponse<T> error(T result) {
+        return of(500, "에러가 발생했습니다.", result);
     }
 }
